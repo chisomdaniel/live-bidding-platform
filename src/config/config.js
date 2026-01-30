3e50d05ab7449d5eea2ad36a905542c5dc74f34e
chisomdaniel <chinwezechisom@gmail.com>
2026-01-30 10:16:26 +0100

    chore: base files and boilerplate code

---
+++ src/config/config.js
@@ -0,0 +1,13 @@
+import dotenv from 'dotenv';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+dotenv.config({ path: path.join(__dirname, '../../.env') });
+
+export default {
+  env: process.env.NODE_ENV,
+  port: process.env.PORT || 3000,
+};
---
+++ src/controllers/health.controller.js
@@ -0,0 +1,6 @@
+import httpStatus from 'http-status';
+import catchAsync from '../utils/catchAsync.js';
+
+export const getHealth = catchAsync(async (req, res) => {
+  res.status(httpStatus.OK).send({ status: 'UP' });
+});
